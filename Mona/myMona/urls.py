2c7cfa8b04c96a6b6d1e85f0d06a0ba8589c2ca8
Daniel <dadeniyi689@gmail.com>
2025-04-21 22:27:18 +0100

    Authentication system implemented

--- Mona/myMona/urls.py
+++ Mona/myMona/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("Login", views.Login, name="Login"),
+    path("Signup", views.Signup, name="Signup"),
+    path("Logout", views.Logout, name="Logout")
 ]
--- Mona/myMona/views.py
+++ Mona/myMona/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 # Create your views here.
 
@@ -8,7 +10,46 @@ def index(request):
     return render(request, "index.html")
 
 def Signup(request):
-    return render(request, "signup.html")
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        pwd = request.POST['password']
+        cpwd = request.POST['confirmPassword']
+        
+        if pwd == cpwd:
+            if User.objects.filter(username=username).exists():
+                messages.info(request, "This username exists... try again")
+                return redirect("Signup")
+            elif User.objects.filter(email=email).exists():
+                messages.info(request, "This email exists... try again")
+                return redirect("Signup")
+            else:
+                user = User.objects.create_user(username=username, password=pwd, email=email)
+                user.save()
+                return redirect("Login")
+        else:
+            return render(request, "signup.html")
+    else:
+        messages.info(request, "Something went wrong... try again")
+        return render(request, "signup.html")
 
 def Login(request):
-    return render()
+    if request.method == 'POST':
+        username = request.POST["username"]
+        password = request.POST["password"]
+        
+        user = authenticate(request, username=username, password=password)
+        # user = User.objects.filter(username=username, password=password).exists()
+        
+        if user is not None:
+            login(request, user)
+            return redirect("/")
+        else:
+            messages.info(request, "Incorrect username or password")
+            return render(request, "login.html")
+    else:
+        return render(request, "login.html")
+
+def Logout(request):
+    logout(request)
+    return redirect("/")
